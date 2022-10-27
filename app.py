ad98a22c4b2a1d6282897b04259ad88b51532e37
Ratasich Denise (BCC.IKT) <denise.ratasich@oebb.at>
2022-10-27 16:37:11 +0200

    Remove opencv image pre processing
    - tesseract works best as is :)
    - format code with `autoflake . && black . && isort .`

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+import argparse
 import os
 import sys
-from PIL import Image
+
 import pytesseract
-import argparse
-import cv2
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 
 __author__ = 'Rick Torzynski <ricktorzynski@gmail.com>'
 __source__ = ''
 
 app = Flask(__name__)
 UPLOAD_FOLDER = './static/uploads'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024
 
 @app.route("/")
@@ -34,30 +33,15 @@ def upload_file():
       # save file to /static/uploads
       filepath = os.path.join(app.config['UPLOAD_FOLDER'],filename)
       f.save(filepath)
-      
-      # load the example image and convert it to grayscale
-      image = cv2.imread(filepath)
-      gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-      
-      # apply thresholding to preprocess the image
-      gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
-      # apply median blurring to remove any blurring
-      gray = cv2.medianBlur(gray, 3)
+      # perform OCR on the image
+      text = pytesseract.image_to_string(filepath)
 
-      # save the processed image in the /static/uploads directory
-      ofilename = os.path.join(app.config['UPLOAD_FOLDER'],"{}.png".format(os.getpid()))
-      cv2.imwrite(ofilename, gray)
-      
-      # perform OCR on the processed image
-      text = pytesseract.image_to_string(Image.open(ofilename))
-      
       # remove the processed image
-      os.remove(ofilename)
+      os.remove(filepath)
 
       return render_template("uploaded.html", displaytext=text, fname=filename)
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
   app.run(debug=True, host='0.0.0.0', port=port)
-  # app.run(host="0.0.0.0", port=5000, debug=True)
